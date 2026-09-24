b9c6f4d5ac707da3cbeb3053e8cae0d59b21a23e
Jesus Abellan <jabegar961@g.educaand.es>
2026-09-24 12:01:22 +0200

    ejercicio 4

--- ejercicios.js
+++ ejercicios.js
@@ -21,5 +21,22 @@ else {
 var num1 = prompt('Inserte un numero');
 var num2 = prompt('Inserte otro numero');
 
-alert(`La suma es: ${num1 + num2}\nLa resta es: ${num1 - num2}\nLa multiplicación es: ${num1 * num2}\nLa división es: ${num1 / num2}`);
+// alert(`La suma es: ${num1 + num2}\nLa resta es: ${num1 - num2}\nLa multiplicación es: ${num1 * num2}\nLa división es: ${num1 / num2}`);
 //-----------------------------------------------------------------
+// EJERCICIO 4
+//-----------------------------------------------------------------
+document.write('<h1>Lenguaje de marcas</h1>');
+
+var notaPractica = parseInt(prompt('Introduce la nota de tus practicas'));
+var notaExamen = parseInt(prompt('Introduce la nota de tu examen'));
+var notaActitud = parseInt(prompt('Introduce la nota de tu actitud'));
+
+var notaMedia = (notaPractica + notaExamen + notaActitud) / 3;
+document.write(`<p>Tu nota media es ${notaMedia.toFixed(2)}</p>`);
+
+if (notaMedia >= 5) {
+    document.write('<p>Enhorabuena! Has aprobado!</p>');
+}
+else {
+    document.write('<p>Vaya! Tendras que ir a recuperacion</p>');
+}
