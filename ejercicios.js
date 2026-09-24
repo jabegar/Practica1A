b962c4eaa45fb426b474d59c6228c0b2c823f1c8
Jesus Abellan <jabegar961@g.educaand.es>
2026-09-24 10:59:51 +0200

    ejercicio 2

--- ejercicios.js
+++ ejercicios.js
@@ -1,3 +1,18 @@
+// EJERCICIO 1
+//-----------------------------------------------------------------
 var nombre = prompt('Escribe aqui tu nombre');
 document.write('<h1>Hola ' + nombre + '</h1>');
 document.write('Tu nombre tiene ' + nombre.length + ' letras');
+//-----------------------------------------------------------------
+// EJERCICIO 2
+//-----------------------------------------------------------------
+var texto = prompt('Escribe letras al azar');
+var num = prompt('Escribe una posicion');
+
+if (num > texto.length) {
+    alert('El numero es demasiado grande');
+}
+else {
+    alert('En la posición ' + num + ' de la cadena ' + texto + ' se encuentra el carácter ' + texto[num]);
+}
+//-----------------------------------------------------------------
