b52e8ea3df7ef19d63b36a31743278fd0192178d
Jesus Abellan <jabegar961@g.educaand.es>
2026-09-24 11:39:52 +0200

    ejercicio 3

--- ejercicios.js
+++ ejercicios.js
@@ -16,3 +16,10 @@ else {
     alert('En la posición ' + num + ' de la cadena ' + texto + ' se encuentra el carácter ' + texto[num]);
 }
 //-----------------------------------------------------------------
+// EJERCICIO 3
+//-----------------------------------------------------------------
+var num1 = prompt('Inserte un numero');
+var num2 = prompt('Inserte otro numero');
+
+alert(`La suma es: ${num1 + num2}\nLa resta es: ${num1 - num2}\nLa multiplicación es: ${num1 * num2}\nLa división es: ${num1 / num2}`);
+//-----------------------------------------------------------------
